03a57bd7bfe3ba24a0e5253f31cc261709f76442
Alanimustafa <mustafa.kamel.ani@gmail.com>
2025-02-20 10:53:59 -0600

    AXIOS Get on Home page has been successfully imported the data from the Backend

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,16 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 function Home () {
+    const [vehicles, setVehicles] = useState([])
+
+    console.log(vehicles)
+    useEffect(()=>{
+        axios.get("http://localhost:3000/")
+        .then(response => {setVehicles(response.data)
+           // console.log("Vehicle Data :",response.data);
+        })
+        .catch((error) => {console.error(error)})
+    },[])
     return (
         <>
         <h1>Home Page</h1>
