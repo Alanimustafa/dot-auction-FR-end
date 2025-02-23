34bfc13cd17f356ef3923e45018ed22eed7140e2
Alanimustafa <mustafa.kamel.ani@gmail.com>
2025-02-23 13:13:51 -0600

    Compact Vehicles Page has been developed and tested successfully

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-// import './index.css'
-import App from './App.jsx'
+ //import './index.css'
+// import App from './App.jsx'
 
 // Routes Modules Imports
 import Home from './Pages/Home.jsx'
