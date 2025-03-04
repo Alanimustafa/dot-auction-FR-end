42ce316e38e036a188fa072ca2bd453e954ad93d
Alanimustafa <mustafa.kamel.ani@gmail.com>
2025-03-04 14:55:52 -0600

    New Vehicle Submision has been updated

--- src/Components/NewVehicle.jsx
+++ src/Components/NewVehicle.jsx
@@ -52,7 +52,7 @@ const AddVehicleForm = () => {
       });
       console.log("Vehicle added:", response.data);
       alert("Vehicle successfully added!");
-      nav(`/${formData.type}s`);
+      nav(`/details/${formData._id}s`);
     } catch (error) {
       console.error("Error adding vehicle:", error);
       alert("Failed to add vehicle");
